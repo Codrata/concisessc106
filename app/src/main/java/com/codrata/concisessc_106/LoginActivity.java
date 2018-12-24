87cf4eac47647abd27c383f38993499d0a8e4c2f
codrata <clintonoye@codrata.com>
2018-12-24 15:29:16 +0100

    new commit

--- app/src/main/java/com/codrata/concisessc_106/LoginActivity.java
+++ app/src/main/java/com/codrata/concisessc_106/LoginActivity.java
@@ -1,12 +1,11 @@
 package com.codrata.concisessc_106;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
-import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
