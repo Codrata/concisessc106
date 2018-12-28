6cff22c40fdc6e2cd813aa52e5b23cb5004ca354
Atobatele Hameed <hakmid4united@gmail.com>
2018-12-28 12:30:48 +0100

    Created Profile Activity
    Profile Activity to display NAME, ACTIVATION STATUS, ACTIVATION DATE, E-MAIL.

--- app/src/main/java/com/codrata/concisessc_106/ProfileActivity.java
+++ app/src/main/java/com/codrata/concisessc_106/ProfileActivity.java
@@ -1,13 +1,46 @@
 package com.codrata.concisessc_106;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_dashboard:
+                    Intent i = new Intent(ProfileActivity.this, NotesActivity.class);
+                    startActivity(i);
+                    overridePendingTransition(R.anim.slideinright, R.anim.slideoutright);
+                    return true;
+
+                case R.id.navigation_notifications:
+
+                    return true;
+
+
+                case R.id.navigation_home:
+                    Intent x = new Intent(ProfileActivity.this, Main2Activity.class);
+                    startActivity(x);
+                    overridePendingTransition(R.anim.slideinright, R.anim.slideoutright);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 }
